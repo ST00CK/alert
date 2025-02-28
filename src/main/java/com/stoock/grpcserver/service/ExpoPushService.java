6b8071c18374df5a5c9e8fc86e34a3b845dee64a
jeenukchung <jeenuk.chung@gmail.com>
2025-02-28 16:06:32 +0900

    Expo 알림 전송 코드

---
+++ src/main/java/com/stoock/grpcserver/service/ExpoPushService.java
@@ -0,0 +1,42 @@
+package com.stoock.grpcserver.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ExpoPushService {
+    private static final String EXPO_PUSH_URL = "https://exp.host/--/api/v2/push/send";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public boolean sendPushNotification(String pushToken, String roomId, String userId, String message) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String title = "채팅방 (" + roomId + ") 새로운 메시지!";
+        String body = userId + ": " + message;
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("to", pushToken);
+        payload.put("title", title);
+        payload.put("body", body);
+        payload.put("sound", "default");
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(payload, headers);
+
+        try{
+            ResponseEntity<String> response = restTemplate.exchange(EXPO_PUSH_URL, HttpMethod.POST, request, String.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                System.err.println("Expo Push Notification ERROR: " + response.getBody());
+            }
+            return response.getStatusCode().is2xxSuccessful();
+        } catch (Exception e) {
+            System.err.println("Expo Push Notification ERROR: " + e.getMessage());
+            return false;
+        }
+    }
+}
